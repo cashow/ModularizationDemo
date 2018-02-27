993139f6a6db23c96ef97d0e91099cf1c7028716
cashow <wy89767@163.com>
2018-02-27 21:38:26 +0800

    引入 ARouter

--- app/src/main/java/com/cashow/modularizationdemo/AndroidApplication.java
+++ app/src/main/java/com/cashow/modularizationdemo/AndroidApplication.java
@@ -17,6 +17,7 @@ package com.cashow.modularizationdemo;
 
 import android.app.Application;
 
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.cashow.modularizationdemo.common.di.components.ApplicationComponent;
 import com.cashow.modularizationdemo.common.di.components.DaggerApplicationComponent;
 import com.cashow.modularizationdemo.common.di.modules.ApplicationModule;
@@ -32,12 +33,21 @@ public class AndroidApplication extends Application {
     public void onCreate() {
         super.onCreate();
         this.initializeInjector();
+        this.initARouter();
     }
 
     private void initializeInjector() {
         this.applicationComponent = DaggerApplicationComponent.builder()
                 .applicationModule(new ApplicationModule(this))
                 .build();
+    }
+
+    private void initARouter() {
+        if (BuildConfig.DEBUG) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            ARouter.openLog();     // 打印日志
+            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(this); // 尽可能早，推荐在Application中初始化
     }
 
     public ApplicationComponent getApplicationComponent() {

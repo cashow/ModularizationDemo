18c0c220698522433cd7edf0899b5e24fb667e9d
cashow <wy89767@163.com>
2018-02-27 22:20:02 +0800

    实现 userlist 模块独立运行

---
+++ userlist/src/main/java/com/cashow/userlist/TestApplication.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright (C) 2014 android10.org. All rights reserved.
+ *
+ * @author Fernando Cejas (the android10 coder)
+ */
+package com.cashow.userlist;
+
+import com.cashow.baselibrary.BaseApplication;
+
+public class TestApplication extends BaseApplication {
+}
